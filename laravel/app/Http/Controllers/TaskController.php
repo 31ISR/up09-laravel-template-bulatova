ea0b6f08a8b1c25228817fe9122d4f169e20bbe4
Yanabulatova <bulatovayana03@gmail.com>
2026-05-25 09:45:48 +0000

    123

--- laravel/app/Http/Controllers/TaskController.php
+++ laravel/app/Http/Controllers/TaskController.php
@@ -2,17 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class TaskController extends Controller
 {
     public function index()
     {
-
+    /**@var User $user*/
+        $user = Auth::user();
+        $tasks = $user->tasks()->with('category')->latest()->paginate(10);
+        return view('tasks.index', compact('tasks'));
     }
 
-    public function store()
+    public function store(Request $request)
     {
-        
+        $validated = $request->validate([
+            "title" => "required|string|max:255",
+            "description" => "|string",
+        ]);
     }
+    
+    public function create()
+    {
+        /**@var User $user*/
+        $user = Auth::user();
+        $categories = $user->categories()->get();
+        return view('tasks.create', compact('categories'));
+    }
+
 }
